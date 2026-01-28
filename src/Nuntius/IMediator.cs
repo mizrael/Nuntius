9a4947d05c1aff09400f31dc6fdfb0718e14409f
Enrico Massone <enrico.massone@hotmail.it>
2026-01-28 22:32:08 +0100

    feat: added XML documentation to public types

--- src/Nuntius/IMediator.cs
+++ src/Nuntius/IMediator.cs
@@ -1,6 +1,8 @@
 ﻿namespace Nuntius;
 
-
+/// <summary>
+/// A mediator which is capable of doing both point to point and pub-sub communication patterns.
+/// </summary>
 public interface IMediator : ISender, IPublisher
 {
 }
--- src/Nuntius/INotificationHandler.cs
+++ src/Nuntius/INotificationHandler.cs
@@ -1,7 +1,28 @@
 ﻿namespace Nuntius;
 
+/// <summary>
+/// Defines a handler for a notification.
+/// </summary>
+/// <typeparam name="TNotification">
+/// The type of notification being handled.
+/// </typeparam>
+/// <remarks>
+/// Use this interface for pub-sub scenarios.
+/// </remarks>
 public interface INotificationHandler<in TNotification>
     where TNotification : INotification
 {
-    ValueTask Handle(TNotification request, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Handles a notification.
+    /// </summary>
+    /// <param name="notification">
+    /// The notification to handle.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Optional cancellation token.
+    /// </param>
+    /// <returns>
+    /// A value task that represents the asynchronous operation.
+    /// </returns>
+    ValueTask Handle(TNotification notification, CancellationToken cancellationToken = default);
 }
